2e71ef0c99f31e0867d20e0f61c97eecee6c0e2b
SwethaChintala321 <schint12@uncc.edu>
2023-09-18 13:34:42 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public1'));
+app.use(cors());
 
 const budget = {
     myBudget: [
@@ -53,15 +54,10 @@ const budget = {
 ]
 };
 
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.get('/budget', (req, res) => {
   res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
